d0b1d0dea76edc7727cb179263a023cea8c5aa05
Gn0lee <jinho605@gmail.com>
2022-02-25 22:06:49 +0900

    .

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
-    cors : {
-        origin : "*",
-        methods : ["GET","POST"]
-    }
+    origin : "*"
 });
 const PORT = process.env.PORT || 3001;
 const path = require('path');
